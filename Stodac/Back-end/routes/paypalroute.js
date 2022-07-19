414f739c09db635eda6728a3e38a1ce9bcc5fd02
Tobraider <96182904+Tobraider@users.noreply.github.com>
2022-07-19 16:02:37 +0200

    Delete Stodac directory

--- Stodac/Back-end/routes/paypalroute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paypalCtrl = require('../controller/paypal');
-
-router.get('/testtoken/', paypalCtrl.testToken)
-// router.get('/demandetoken/', paypalCtrl.demandeToken)
-
-module.exports = router;
