f97e6b9cab16e1ef10e21d44d8c3edd7ba721a93
Tobraider <96182904+Tobraider@users.noreply.github.com>
2022-07-19 16:02:13 +0200

    Delete Back-end directory

--- Back-end/routes/facture.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const factureCtrl = require('../controller/facture');
-const auth = require('../middleware/auth')
-//const authAdmin = require('../middleware/auth-admin')
-
-router.get('/create/:id', auth, factureCtrl.createFactue)
-
-module.exports = router;
